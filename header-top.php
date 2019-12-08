7387f41deb7918aa7583358310a7bb8db6667808
yuya-inagaki <yuya.ina56@gmail.com>
2019-12-08 23:52:29 +0900

    トップスライダーをテンプレート化

--- header-top.php
+++ header-top.php
@@ -84,7 +84,7 @@ Template Name: HD, HOME
                 if (!empty($header_image)) : ?>
                     <a href="<?php echo home_url(); ?>"><img class="header_logo" src="<?php header_image(); ?>" alt="<?php bloginfo('name'); ?>"></a>
                 <?php else: ?>
-                    <a href="<?php echo home_url('/'); ?>"><img class="header_logo" src="<?php bloginfo('template_url'); ?>/img/JAFS-see.png" alt="JAFS logo"></a>
+                    <a href="<?php echo home_url('/'); ?>"><img class="header_logo" src="<?php bloginfo('template_url'); ?>/img/JAFS-see.png" alt="e-kagaku logo"></a>
                 <?php endif; ?>
                 <!-- //ヘッダーロゴ -->
                 <!-- ハンバーガーメニュー -->
@@ -115,12 +115,7 @@ Template Name: HD, HOME
         <!-- //モバイル用ヘッダーメニュー -->
         
         <!-- トップページメインスライダー -->
-        <div class="top-slider">
-            <a href="http://global-science.or.jp/src/" target="_blank"><div class="contents slick-slide"><div class="contents_img" style="background:url('<?php echo get_template_directory_uri(); ?>/img/slideshow/top01.jpg'); background-position:center center; background-size:cover;"></div><div class="text"><div class="text_large"><span>スペース</span><br><span>ロボットコンテスト</span></div><div class="text_small"><span>月面基地をイメージしたコースに挑戦しよう！</span></div></div></div></a>
-            <a href="http://global-science.or.jp/grsc/" target="_blank"><div class="contents slick-slide"><div class="contents_img" style="background:url('<?php echo get_template_directory_uri(); ?>/img/slideshow/top04.jpg'); background-position:center center; background-size:cover;"></div><div class="text"><div class="text_large"><span>国際・国内科学合宿</span></div><div class="text_small"><span>本格的な科学研究にチャレンジ！</span></div></div></div></a>
-            <a href="http://e-kagaku.com/top_general/curriculum/"><div class="contents slick-slide"><div class="contents_img" style="background:url('<?php echo get_template_directory_uri(); ?>/img/slideshow/top02.png'); background-position:center center; background-size:cover;"></div><div class="text"><div class="text_large"><span>JAFS</span><br><span>カリキュラム</span></div><div class="text_small"><span>ステップアップ型のカリキュラムでしっかり学習</span></div></div></div></a>
-            <a href="http://global-science.or.jp/cafe/" target="_blank"><div class="contents slick-slide"><div class="contents_img" style="background:url('<?php echo get_template_directory_uri(); ?>/img/slideshow/top03.png'); background-position:center center; background-size:cover;"></div><div class="text"><div class="text_large"><span>北原代表の</span><br><span>ブログOPEN</span></div><div class="text_small"><span>e - k a g a k u  c a f e</span></div></div></div></a>
-        </div>
+        <?php get_template_part('template/top-slider'); ?>
         <!-- //トップページメインスライダー -->
         
         <!-- トップページ最重要ニュース -->
