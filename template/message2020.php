1b1150d49854bb679e5e3f7c8732af6fcaea80c2
Yuya Inagaki <yuya1756@live.jp>
2020-01-13 14:03:49 +0900

    bug fixed

--- template/message2020.php
+++ template/message2020.php
@@ -19,7 +19,7 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-6 offset-sm-3">
-                <a taget="_blank" href="https://docs.google.com/forms/d/16DdMVnZbYBt2Zo9KU4F4HavemqDWvJCywJPgw4nn8t4/viewform"><img width="100%" src="<?php echo get_template_directory_uri(); ?>/imag/toppage/banner_info.jpg"></a>
+                <a taget="_blank" href="https://docs.google.com/forms/d/16DdMVnZbYBt2Zo9KU4F4HavemqDWvJCywJPgw4nn8t4/viewform"><img width="100%" src="<?php echo get_template_directory_uri(); ?>/img/toppage/banner_info.jpg"></a>
             </div>
         </div>
     </div>
