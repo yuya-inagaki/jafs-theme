f6373606ce400aaaf6fccb360eb376638ac4ee18
Yuya Inagaki <yuya1756@live.jp>
2020-01-13 14:05:03 +0900

    bug fixed

--- template/message2020.php
+++ template/message2020.php
@@ -17,9 +17,9 @@
   </div>
   <div style="margin:10px 0;">
     <div class="container">
-        <div class="row">
+        <div class="row text-center">
             <div class="col-sm-6 offset-sm-3">
-                <a taget="_blank" href="https://docs.google.com/forms/d/16DdMVnZbYBt2Zo9KU4F4HavemqDWvJCywJPgw4nn8t4/viewform"><img width="100%" src="<?php echo get_template_directory_uri(); ?>/img/toppage/banner_info.jpg"></a>
+                <a target="_blank" href="https://docs.google.com/forms/d/16DdMVnZbYBt2Zo9KU4F4HavemqDWvJCywJPgw4nn8t4/viewform"><img width="100%" src="<?php echo get_template_directory_uri(); ?>/img/toppage/banner_info.jpg"></a>
             </div>
         </div>
     </div>
