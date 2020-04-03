cc96602970498dd18d3674dc2d7b6aeeff0cf3cc
Yuya Inagaki <yuya1756@live.jp>
2020-04-04 03:59:15 +0900

    add btn

---
+++ template/enkaku-shiryou-btn.php
@@ -0,0 +1,40 @@
+<div class="apply-btn">
+  <div class="container text-center">
+    <p>お申し込み・お問い合わせはこちら</p>
+    <div style="max-width: 600px; margin: 0 auto;">
+      <div class="row">
+        <div class="col-6">
+          <a class="btn" href="https://forms.gle/WTs3s6TT9bVo7UfG7" target="_blank">個人<span>の方はこちら</span></a></div>
+        <div class="col-6">
+          <a class="btn orange" href="https://forms.gle/3ATnKtGpWN9nh9ne8" target="_blank">学校<span>の方はこちら</span></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style type="text/css">
+  .apply-btn {
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 1;
+    background: rgba(0, 0, 0, 0.8);
+    padding: 10px 0 0;
+  }
+
+  .apply-btn p {
+    color: white;
+  }
+
+  @media only screen and (max-width: 874px) {
+    a.btn {
+      padding: 5px 10px;
+    }
+  }
+
+  .btn span {
+    font-size: 0.7rem;
+  }
+</style>
